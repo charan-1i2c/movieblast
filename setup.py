12e05fdda09c3805d2b6e46db048704504acd696
sishta charan <sishtassc@gmail.com>
2026-04-06 19:53:36 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
@@ -6,12 +6,11 @@ with open("README.md", "r", encoding="utf-8") as f:
 ## edit below variables as per your requirements -
 REPO_NAME = "Movie-Recommender-System-Using-Machine-Learning"
 AUTHOR_USER_NAME = "entbappy"
-SRC_REPO = "src"
+# SRC_REPO = "src"  # Remove or comment this line if your code isn't in 'src'
 LIST_OF_REQUIREMENTS = ['streamlit']
 
-
 setup(
-    name=SRC_REPO,
+    name=REPO_NAME,  # Change from SRC_REPO to your repo name
     version="0.0.1",
     author=AUTHOR_USER_NAME,
     description="A small package for Movie Recommender System",
@@ -19,7 +18,8 @@ setup(
     long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     author_email="entbappy73@gmail.com",
-    packages=[SRC_REPO],
+    # packages=[SRC_REPO],  # Remove this line
+    packages=find_packages(),  # Use find_packages() to discover all packages in the root directory
     license="MIT",
     python_requires=">=3.7",
     install_requires=LIST_OF_REQUIREMENTS
