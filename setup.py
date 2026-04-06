1cd1c5eee9367185d541aecb1958941c7a767b6e
sishta charan <sishtassc@gmail.com>
2026-04-06 20:17:25 +0530

    Update setup.py to use SRC_REPO as package name

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
@@ -6,11 +6,12 @@ with open("README.md", "r", encoding="utf-8") as f:
 ## edit below variables as per your requirements -
 REPO_NAME = "Movie-Recommender-System-Using-Machine-Learning"
 AUTHOR_USER_NAME = "entbappy"
-# SRC_REPO = "src"  # Remove or comment this line if your code isn't in 'src'
+SRC_REPO = "src"
 LIST_OF_REQUIREMENTS = ['streamlit']
 
+
 setup(
-    name=REPO_NAME,  # Change from SRC_REPO to your repo name
+    name=SRC_REPO,
     version="0.0.1",
     author=AUTHOR_USER_NAME,
     description="A small package for Movie Recommender System",
@@ -18,8 +19,7 @@ setup(
     long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     author_email="entbappy73@gmail.com",
-    # packages=[SRC_REPO],  # Remove this line
-    packages=find_packages(),  # Use find_packages() to discover all packages in the root directory
+    packages=[SRC_REPO],
     license="MIT",
     python_requires=">=3.7",
     install_requires=LIST_OF_REQUIREMENTS
